03211b23d656b5c71c64c9c08fb830337ddf009e
Craft&Star <1633696870@qq.com>
2021-01-04 14:13:46 +0800

    java 基础知识测试

--- src/test/test/Test2.java
+++ src/test/test/Test2.java
@@ -1,2 +1,11 @@
+import org.junit.Test;
+
 public class Test2 {
+
+    @Test
+    public void testAdd(){
+        int a=3,b=4;
+        int result =Math.floorDiv(3,4);
+        System.out.println(result);
+    }
 }
