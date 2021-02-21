54eb7367d3d93c0fcc9a1e88e9bba9b10d03b0a2
mac <1633696870@qq.com>
2021-02-21 17:56:06 +0800

    label标签使用

--- src/test/lambda/lambda/KnowledgeTest.java
+++ src/test/lambda/lambda/KnowledgeTest.java
@@ -19,32 +19,32 @@ public class KnowledgeTest {
 
     //强引用
     @Test
-    public void testStrongRef(){
+    public void testStrongRef() {
         Object object = strongRef;
-        strongRef=null;
-        object=null;
+        strongRef = null;
+        object = null;
         System.gc();
-        System.out.println("GC之后:"+object);
+        System.out.println("GC之后:" + object);
     }
 
     //软引用
     @Test
-    public void testSoftRef(){
-        String str ="hello";
+    public void testSoftRef() {
+        String str = "hello";
         Object softRef = new Object();
         SoftReference softReference = new SoftReference(str);
         softRef = null;
-        str=null;
+        str = null;
         System.out.println(softReference.get());
     }
 
     //弱引用
     @Test
-    public void testWeakRef(){
+    public void testWeakRef() {
         Object weakObject = new Object();
         WeakReference weakReference = new WeakReference(weakObject);
         System.gc();
-        System.out.println("After:"+weakReference.get());
+        System.out.println("After:" + weakReference.get());
     }
 
     //虚引用
@@ -52,7 +52,7 @@ public class KnowledgeTest {
     public void testPhantomRef() throws InterruptedException {
         ReferenceQueue referenceQueue = new ReferenceQueue();
         Object phantomObj = new Object();
-        PhantomReference phantomReference = new PhantomReference(phantomObj,referenceQueue);
+        PhantomReference phantomReference = new PhantomReference(phantomObj, referenceQueue);
         System.out.println(phantomReference.get());
 
         phantomObj = null;
@@ -73,10 +73,10 @@ public class KnowledgeTest {
      **/
     @Test
     public void testInteger() throws NoSuchFieldException, IllegalAccessException {
-        Integer a=1,b=2;
-        System.out.println("Before:a="+a+",b="+b);
-        swap(a,b);
-        System.out.println("After:a="+a+",b="+b);
+        Integer a = 1, b = 2;
+        System.out.println("Before:a=" + a + ",b=" + b);
+        swap(a, b);
+        System.out.println("After:a=" + a + ",b=" + b);
     }
 
     /**
@@ -87,7 +87,7 @@ public class KnowledgeTest {
      * @Author: lxc
      * @Date: 2020/2/5 15:26
      **/
-    public static void swap(Integer i1,Integer i2) throws NoSuchFieldException, IllegalAccessException {
+    public static void swap(Integer i1, Integer i2) throws NoSuchFieldException, IllegalAccessException {
 //        i1 = i1+i2;//i1=1+2=3
 //        i2 = i1-i2;//i2=3-2=1
 //        i1 = i1-i2;//i1=3-1=2
@@ -95,8 +95,8 @@ public class KnowledgeTest {
         field.setAccessible(true);
 //        int tmp = i1.intValue();
         Integer tmp = new Integer(i1.intValue());
-        field.setInt(i1,i2.intValue());
-        field.setInt(i2,tmp);
+        field.setInt(i1, i2.intValue());
+        field.setInt(i2, tmp);
     }
 
     /**
@@ -131,7 +131,7 @@ public class KnowledgeTest {
         car.setCarPrice("21");
 
         Car car1 = car.deepClone();
-        System.out.println(car1.getCarColor()+"->"+car1.getCarPrice());
+        System.out.println(car1.getCarColor() + "->" + car1.getCarPrice());
     }
 
     /**
@@ -143,70 +143,110 @@ public class KnowledgeTest {
      * @Date: 2020/2/6 17:55
      **/
     @Test
-    public void testEquals(){
-        String a="x",b="x";
+    public void testEquals() {
+        String a = "x", b = "x";
         String c = new String("x");
-        System.out.println(a==b);
+        System.out.println(a == b);
         System.out.println(a.equals(b));
         System.out.println(a.equals(c));
-        System.out.println(a==c);
+        System.out.println(a == c);
     }
 
     @Test
-    public void testCal(){
-        int a=3;
+    public void testCal() {
+        int a = 3;
         System.out.println(++a);
     }
 
     @Test
-    public void testMap(){
-        Map<String,Object> map = new HashMap<String,Object>();
+    public void testMap() {
+        Map<String, Object> map = new HashMap<String, Object>();
 
-        map.put("1","jack");
-        map.put("2","tom");
-        map.put("3","tina");
+        map.put("1", "jack");
+        map.put("2", "tom");
+        map.put("3", "tina");
 
         System.out.println(map.get("1").hashCode());
     }
 
     private static int i;
     private volatile boolean isStop = false;
+
     @Test
     public void testThread() throws InterruptedException {
-        Thread thread = new Thread(()->{
-            while (!isStop){
+        Thread thread = new Thread(() -> {
+            while (!isStop) {
                 i++;
-                System.out.println("num:->"+i);
+                System.out.println("num:->" + i);
             }
         });
 
         thread.start();
         TimeUnit.SECONDS.sleep(2);
-        isStop=true;
+        isStop = true;
         System.out.println("Thread is stoped!");
     }
 
     @Test
-    public void testFloat(){
-        System.out.println(Math.round(11.5)+"   "+Math.round(-11.5));
+    public void testFloat() {
+        System.out.println(Math.round(11.5) + "   " + Math.round(-11.5));
     }
 
     @Test
-    public void testAssert(){
+    public void testAssert() {
         int age = -21;
         this.testAssert(age);
     }
 
-    public void testAssert(int arg){
-        assert arg>0:"The age is ilegale";
-        System.out.println("continue: Your age is:"+arg);
+    public void testAssert(int arg) {
+        assert arg > 0 : "The age is ilegale";
+        System.out.println("continue: Your age is:" + arg);
     }
 
     @Test
-    public void testBoolean(){
-        int a=6,b=4;
+    public void testBoolean() {
+        int a = 8, b = 4, c = 2 << 3;
 
-        String str = a>5 || b<5?"true":"false";
+        String str = a == c || b < 5 ? "true" : "false";
         System.out.println(str);
+    }
+
+    @Test
+    public void testLable() {
+        first:
+        // 标签与循环直接不可以加语句
+        for (int i = 0; i < 3; ++i) {
+            System.out.println("\nfirst: " + (i + 1) + " times start");
+
+            second:
+            for (int j = 0; j < 2; ++j) {
+                System.out.println("\tsecond: " + (j + 1) + " times start");
+                if (i == 1 && j == 1) {
+                    System.out.println("\t----break----");
+                    break;
+                }
+
+
+                for (int k = 0; k < 2; ++k) {
+                    System.out.println("\t\tthird: " + (k + 1) + " times start");
+                    if (i == 1 && j == 0 && k == 0) {
+                        System.out.println("\t\t----continue----");
+                        continue;
+                    }
+                    if (i == 2 && j == 0) {
+                        System.out.println("\t\t----continue second----");
+                        continue second;
+                    }
+                    if (i == 2 && j == 1 && k == 1) {
+                        System.out.println("\t\t----break first----");
+                        break first;
+                    }
+
+                    System.out.println("\t\tthird: " + (k + 1) + " times end");
+                }
+                System.out.println("\tsecond: " + (j + 1) + " times end");
+            }
+            System.out.println("first: " + (i + 1) + " times end");
+        }
     }
 }
